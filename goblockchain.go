1aa31ef21caf1dae5fc3d6e64aca03c3cd7692b5
Shou Tseng <yakushou730@gmail.com>
2022-02-12 23:35:00 +0800

    feat: create a blockchain structure

--- goblockchain.go
+++ goblockchain.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -28,11 +29,38 @@ func (b *Block) Print() {
 	fmt.Printf("transactions      %s\n", b.transactions)
 }
 
+type Blockchain struct {
+	transactionPool []string
+	chain           []*Block
+}
+
+func NewBlockchain() *Blockchain {
+	bc := new(Blockchain)
+	bc.CreateBlock(0, "Init hash")
+	return bc
+}
+
+func (bc *Blockchain) CreateBlock(nonce int, previousHash string) *Block {
+	b := NewBlock(nonce, previousHash)
+	bc.chain = append(bc.chain, b)
+	return b
+}
+
+func (bc *Blockchain) Print() {
+	for i, block := range bc.chain {
+		fmt.Printf("%s Chain %d %s\n", strings.Repeat("=", 25), i, strings.Repeat("=", 25))
+		block.Print()
+	}
+	fmt.Printf("%s\n", strings.Repeat("*", 25))
+}
+
 func init() {
 	log.SetPrefix("Blockchain: ")
 }
 
 func main() {
-	b := NewBlock(0, "init hash")
-	b.Print()
+	blockChain := NewBlockchain()
+	blockChain.CreateBlock(5, "hash 1")
+	blockChain.CreateBlock(2, "hash 2")
+	blockChain.Print()
 }
